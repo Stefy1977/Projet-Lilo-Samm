812ca97d4be875ab43cc04dae5955e1846d385ed
Stefy1977 <steftechnocite@gmail.com>
2022-02-05 07:43:10 +0100

    pagination shop.html + scroll top main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+// on va chercher notre bouton
+
+monBouton = document.getElementById("scroll");
+
+// quand on scroll à plus de 20 px à partir du haut
+
+window.onscroll = function(){
+    scrollFunction()
+};
+
+function scrollFunction() {
+    // le 1er c'est pour Safari     et  le 2e pour les autres navigateurs
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) { 
+        monBouton.style.display = "block"
+    } else {
+        monBouton.style.display = "none"
+    }
+}
+
+// quand l'utilisateur clique sur le bouton il va scroller vers le haut (top) de la page !
+
+function topFunction() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome...
+}
