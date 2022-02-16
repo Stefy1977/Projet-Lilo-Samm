58a86edb45606f90b5e981cb0f1afe2aa3a20802
Stefy1977 <95749099+Stefy1977@users.noreply.github.com>
2022-02-16 11:09:35 +0100

    nav (menu) corrigé + mediaqueries

--- js/header.js
+++ js/header.js
@@ -3,6 +3,9 @@ class MyHeader extends HTMLElement {
         const current = this.getAttribute('current');
         this.innerHTML = `<header>
         <nav>
+            <div id="burger">
+                <button type="button">Menu</button>
+            </div>
             <a href="index.html" title="les ateliers de lilo"><img class="logo" src="img/ladl-logo-dark.svg" alt="logo"></a>
             <ul class="menuDesktop">
                 <li><a class="${current === "accueil" ? 'active' : ''}" href="index.html">Accueil</a></li>
@@ -36,37 +39,6 @@ class MyHeader extends HTMLElement {
                 <a href="#" title="me connecter au profil"><img src="img/mdi_account-circle-outline.svg"
                         alt="icône du profil pour se connecter"></a>
             </div>
-
-<!-- nav hamburger -->
-            <div class="hamburger">
-                <div id="burger">
-                    <button type="button">Menu</button>
-                </div>
-                <ul class="menuBurger">
-                    <li><a href="index.html">Accueil</a></li>
-                    <li><a href="#">Boutique</a>
-                        <ul class="subBurger">
-                            <li><a href="shop.html">Voir tout</a></li>
-                            <li><a href="bijoux.html">Bijoux</a></li>
-                            <li><a href="badges.html">Badges</a></li>
-                            <li><a href="papeterie.html">Papeterie</a></li>
-                            <li><a href="nouveautes.html">Nouveautés</a></li>
-                            <li><a href="promo.html">Promo</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="apropos.html">A propos</a></li>
-                    <li><a href="blog.html">Blog</a>
-                        <ul class="subBurger">
-                            <li><a href="blog.html">Actualités</a></li>
-                            <li><a href="evenements.html">Evènements</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="ateliers.html">Ateliers</a></li>
-                    <li><a href="pointsdevente.html">Points de vente</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
-            </div>
-<!-- fin nav hamburger -->
         </nav>
 
     </header>`
